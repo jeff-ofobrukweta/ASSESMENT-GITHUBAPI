8a8ce752753d794b672467b215c090aac5704eb4
jeff-ofobrukweta <oghenerukevwejeff@gmail.com>
2021-06-14 03:29:50 +0100

    chore(repository):truncate text on card with util

--- src/page/home/home.tsx
+++ src/page/home/home.tsx
@@ -6,14 +6,13 @@ import { useLocation } from "react-router";
 import request from "../../api.service/axios.factory";
 import { DEVELOPERS } from "../../routes/constant";
 import "./home.css";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 import { Ideveloper, Ipopular_repository, IqueryReactOption } from "./home.td";
 import { getDevelopers, getReposirories } from "../../utility/github";
 
 const Home = ({ match }: any) => {
   const queryCli = useQueryClient();
-
 
   const useQueryParams = () => {
     return new URLSearchParams(useLocation().search);
@@ -31,7 +30,9 @@ const Home = ({ match }: any) => {
         ""
       );
       return data;
-    } catch (err) {throw new Error(err)}
+    } catch (err) {
+      throw new Error(err);
+    }
   };
 
   const since = query.get("since");
@@ -81,6 +82,10 @@ const Home = ({ match }: any) => {
               data?.map((item: any, index: any) => (
                 <ListCardRepository {...item} key={index} />
               ))}
+
+            {isSuccess && data && data.length <= 1 && (
+              <div>There are no availiable data for this search</div>
+            )}
 
             {/* on-successful state for  developers from react-query */}
             {isSuccess &&
